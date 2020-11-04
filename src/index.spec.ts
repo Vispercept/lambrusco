00960cd7be5ab4795cbb491153cfbb7fe00c7a60
Thomas König <mail@koenigthomas.de>
2020-11-04 15:01:32 +0100

    spy

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,22 +1,23 @@
-import lambrusco from './index'
-
-type Response = Record<string, unknown> | string
+import lambrusco, {Response} from './index'
 
 test('handles uris to correct handler', async () => {
+  const route1 = {
+    pattern: '/anohterhandler/:id',
+    handler: async (): Promise<Response> => 'ERROR!',
+  }
+  const route2 = {
+    pattern: '/test/:id',
+    handler: async (params: {id: string}): Promise<Response> =>
+      params.id,
+  }
+  const spy1 = jest.spyOn(route2, 'handler')
+  const spy2 = jest.spyOn(route2, 'handler')
+
   const app = await new lambrusco({
-    routes: [
-      {
-        pattern: '/test/:id',
-        handler: async (params: {id: string}): Promise<Response> =>
-          params.id,
-      },
-      {
-        pattern: '/test/:id',
-        handler: async (params: {id: string}): Promise<Response> =>
-          params.id,
-      },
-    ],
+    routes: [route1, route2],
   })
   const x = await app.handle('/test/1234')
   expect(x).toStrictEqual('1234')
+  expect(spy1).not.toHaveBeenCalled
+  expect(spy2).toHaveBeenCalled
 })
